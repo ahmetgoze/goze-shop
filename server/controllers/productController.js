5856a53f97aa16dcb1f6e90920e6aeb3fb349d03
ahmetgoze <ahmetgoze90@gmail.com>
2021-09-25 19:40:27 +0300

    Created controllers and routes to create and update products

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -37,3 +37,47 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+// Create a product
+// POST /api/products
+// @access Private
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// Update a product
+// PUT /api/products/:id
+// @access Private
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, countInStock } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
